6d03689fea00aa97c990fa5e3ac68904c28918e6
pushkar <pushkarishwar@gmail.com>
2021-10-02 21:09:07 +0530

    Intro Page done

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,9 +1,42 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: [
+        "Student",
+        "Learner",
+        "Developer",
+        "Working Professional",
+        "Technical Consultant",
+      ],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
-      hbgy
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/intro.jpg" alt="Intro Inage " />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Pushkar</h1>
+          <h3>
+            a <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down-arrow.png" alt="down"></img>
+        </a>
+      </div>
     </div>
   );
 }
--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      dshdsh
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Hybrid Multi Cloud</li>
+        <li>Backend </li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="http://simpleicons.org/icons/mysql.svg" alt="" />
+          <h3>React Dashboard</h3>
+        </div>
+      </div>
     </div>
   );
 }
