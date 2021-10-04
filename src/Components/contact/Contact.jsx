738a2d41bac19f55637328594313216bed10b77c
pushkar <pushkarishwar@gmail.com>
2021-10-04 23:05:58 +0530

    All pages completed

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
+  const [message, setmessage] = useState(false);
+  // const path = [
+  //   {
+  //     pth: "assets/Contact/chatbot.png",
+  //   },
+  //   {
+  //     pth: "assets/Contact/hiring-people.png",
+  //   },
+  //   {
+  //     pth: "assets/Contact/settings-colored.png",
+  //   },
+  // ];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setmessage(true);
+  };
   return (
     <div className="contact" id="contact">
-      hfcfd
+      <div className="left">
+        <img src="assets/Contact/chatbot.png" alt="" />
+
+        {/* {path.map((p) => {
+          return <img src={p.pth} alt="" />;
+        })} */}
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="message" />
+          <button type="Submit">Send</button>
+          {message && <span>Thank you</span>}
+        </form>
+      </div>
     </div>
   );
 }
