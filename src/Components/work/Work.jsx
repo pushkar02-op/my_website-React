dd181416d3000ea86c199e0a9a0dcd66fa845a28
pushkar <pushkarishwar@gmail.com>
2021-10-06 23:48:26 +0530

    made more Mobile Responsive with swipable feature added

--- src/Components/work/Work.jsx
+++ src/Components/work/Work.jsx
@@ -1,83 +1,30 @@
 import { useState } from "react";
 import "./work.scss";
+import { data } from "../../data";
+import { useSwipeable } from "react-swipeable";
 
 export default function Work() {
   const [currentSlide, setcurrentSlide] = useState(0);
-  const data = [
-    {
-      id: "1",
-      icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
-      title: "Hybrid Cloud using Terraform",
-      desc: "Deploy Wordpress+Mysql on AWS and Create a NAT Gateway and Bastion host for management. ",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6724415128483225600",
-    },
-    {
-      id: "2",
-      icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
-      title: "Hybrid Cloud using Terraform",
-      desc: "Hybrid Multi Cloud Setup- Wordpress on Kubernetes and data stored in AWS RDS.",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6721847584970878977",
-    },
-    {
-      id: "3",
-      icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
-      title: "Hybrid Cloud using Terraform",
-      desc: "Deploy Wordpress+MySql on AWS in our own VPC.",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6707302359632015360",
-    },
-    {
-      id: "4",
-      icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
-      title: "Hybrid Cloud using Terraform",
-      desc: "Infrastructure as a Code(IAAC) using AWS Services and TERRAFORM + GITHUB.(using EFS instead of EBS)",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6701448420307066880",
-    },
-    {
-      id: "5",
-      icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
-      title: "Hybrid Cloud using Terraform",
-      desc: "Infrastructure as a Code(IAAC) using AWS Services and TERRAFORM + GITHUB.",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6678197101538754560",
-    },
-    {
-      id: "6",
-      icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
-      title: "Aws EKS Cluster",
-      desc: "Implementing EKS Cluster with different AWS Services Like EC2 , EFS, ELB and EBS.",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6689779072286937088",
-    },
-    {
-      id: "7",
-      icon: "https://camo.githubusercontent.com/c823b22386c31361d21a4c8e76aa8c7f01b2119a0e4cca6fdcf267001735b94e/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616e7369626c652e737667",
-      title: "Ancible AWS",
-      desc: "Ansible-: Playbook to Deploy Web Server on AWS ",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6703998445486714880",
-    },
-    {
-      id: "8",
-      icon: "https://camo.githubusercontent.com/c823b22386c31361d21a4c8e76aa8c7f01b2119a0e4cca6fdcf267001735b94e/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616e7369626c652e737667",
-      title: "Ancible Docker",
-      desc: "Ansible- Playbook to setup web Apache server on Docker",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6702919435042254848",
-    },
-    {
-      id: "9",
-      icon: "https://camo.githubusercontent.com/986c248e8a100a55084cfb249ef904dcb0d6992b3ad36cc43455005301a902c9/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f666c75747465722e737667",
-      title: "Flutter Music App",
 
-      desc: "Flutter App Development- Create a basic Music App with Play, Pause, Stop and Forward Buttons",
-      link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6699265049401323521",
-    },
-  ];
+  const handlers = useSwipeable({
+    onSwipedLeft: () => handleClick("right"),
+    onSwipedRight: () => handleClick("left"),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
 
   const handleClick = (way) => {
     way === "left"
       ? setcurrentSlide(currentSlide > 0 ? currentSlide - 1 : data.length - 1)
       : setcurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
   };
+  const openLink = (source) => {
+    window.open(source, "mywindow");
+  };
   return (
     <div className="work" id="work">
       <div
+        {...handlers}
         className="slider"
         style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
       >
@@ -91,20 +38,22 @@ export default function Work() {
                   </div>
                   <h2>{d.title}</h2>
                   <p>{d.desc}</p>
-                  {/* <span>Projects</span> */}
+                  <button>
+                    <span onClick={() => openLink(d.source)}>Read Blog</span>
+                  </button>
                 </div>
               </div>
               <div className="right">
-                {/* <img src={d.img} alt="" /> */}
-                <iframe
+                <img src={d.link} alt="" />
+                {/* <iframe
                   src={d.link}
                   height="400"
                   width="530"
                   frameBorder="0"
                   allowFullScreen={true}
                   title="Blog post"
-                  // scrolling="no"
-                ></iframe>
+                  scrolling="no"
+                ></iframe> */}
               </div>
             </div>
           </div>
--- src/data.js
+++ src/data.js
@@ -143,3 +143,87 @@ export const devopsPortfolio = [
     link: "https://github.com/pushkar02-op/node-docker",
   },
 ];
+
+export const data = [
+  {
+    id: "1",
+    icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
+    title: "Hybrid Cloud using Terraform",
+    desc: "Deploy Wordpress+Mysql on AWS and Create a NAT Gateway and Bastion host for management. ",
+    link: "https://media-exp1.licdn.com/dms/image/C4E12AQFmGAwq2o-egA/article-cover_image-shrink_423_752/0/1603225374361?e=1639008000&v=beta&t=fzeKibN4gUxpTD97yt-afQm3qIokSq96c8j9kObzsnY",
+    source:
+      "https://www.linkedin.com/pulse/deploy-wordpressmysql-aws-create-nat-gateway-bastion-host-kumar",
+  },
+  {
+    id: "2",
+    icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
+    title: "Hybrid Cloud using Terraform",
+    desc: "Hybrid Multi Cloud Setup- Wordpress on Kubernetes and data stored in AWS RDS.",
+    link: "https://media-exp1.licdn.com/dms/image/C4E12AQEdRDZK8Twezg/article-cover_image-shrink_720_1280/0/1602613232293?e=1639008000&v=beta&t=1YBdm9qHgqJcDEOLPiFnZ5lSCM0gueFd6j4hG5AT2Wo",
+    source:
+      "https://www.linkedin.com/pulse/hybrid-multi-cloud-setup-wordpress-deployed-kubernetes-pushkar-kumar",
+  },
+  {
+    id: "3",
+    icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
+    title: "Hybrid Cloud using Terraform",
+    desc: "Deploy Wordpress+MySql on AWS in our own VPC.",
+    link: "https://media-exp1.licdn.com/dms/image/C4E12AQHS-xG-2OThMg/article-cover_image-shrink_423_752/0/1599141722538?e=1639008000&v=beta&t=lKHtWu-yzFWS8TjbROeKBmv7Yk8sG60VcDkz4NKfUl4",
+    source:
+      "https://www.linkedin.com/pulse/deploy-wordpressmysql-aws-our-own-vpctask-3-pushkar-kumar",
+  },
+  {
+    id: "4",
+    icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
+    title: "Hybrid Cloud using Terraform",
+    desc: "Infrastructure as a Code(IAAC) using AWS Services and TERRAFORM + GITHUB.(using EFS instead of EBS)",
+    link: "https://media-exp1.licdn.com/dms/image/C4E12AQFGsrdHIiosFQ/article-cover_image-shrink_720_1280/0/1597748470733?e=1639008000&v=beta&t=jsrAC-tysnKSNlZNkYX7Y5AYWjvdPPxguQEKPSHcmDE",
+    source:
+      "https://www.linkedin.com/pulse/task-2-infrastructure-codeiaac-using-aws-services-terraform-kumar",
+  },
+  {
+    id: "5",
+    icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
+    title: "Hybrid Cloud using Terraform",
+    desc: "Infrastructure as a Code(IAAC) using AWS Services and TERRAFORM + GITHUB.",
+    link: "https://media-exp1.licdn.com/dms/image/C5612AQFrT07-0nsNXQ/article-cover_image-shrink_720_1280/0/1592164152633?e=1639008000&v=beta&t=vDve3aS5xvn8mAM0o6yQMSJXj9jQdWUby5sLW9nyHxw",
+    source:
+      "https://www.linkedin.com/pulse/infrastructure-codeiaac-using-aws-services-terraform-github-kumar",
+  },
+  {
+    id: "6",
+    icon: "https://camo.githubusercontent.com/f54bd475c0abe0d8dfd979c73d81ea0ee7b3fc0fae894966a64797bcc2b09e7a/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616d617a6f6e6177732e737667",
+    title: "Aws EKS Cluster",
+    desc: "Implementing EKS Cluster with different AWS Services Like EC2 , EFS, ELB and EBS.",
+    link: "https://media-exp1.licdn.com/dms/image/C4E12AQHJTPGTf6HoGw/article-cover_image-shrink_423_752/0/1594958035249?e=1639008000&v=beta&t=klDCNpJ8qo9uKMSmANkWa-oaK6sRrcWvTTnEE6BGJXc",
+    source:
+      "https://www.linkedin.com/pulse/implementing-eks-cluster-different-aws-services-like-ec2-kumar",
+  },
+  {
+    id: "7",
+    icon: "https://camo.githubusercontent.com/c823b22386c31361d21a4c8e76aa8c7f01b2119a0e4cca6fdcf267001735b94e/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616e7369626c652e737667",
+    title: "Ancible AWS",
+    desc: "Ansible-: Playbook to Deploy Web Server on AWS ",
+    link: "https://media-exp1.licdn.com/dms/image/C4E12AQER8QQeAoYQ0w/article-cover_image-shrink_423_752/0/1598353859770?e=1639008000&v=beta&t=gNu2MFkERWBzkAJ-RWKChJ5qSvoS6OrjZfeNtNmdOVI",
+    source:
+      "https://www.linkedin.com/pulse/deploy-web-server-aws-through-ansible-retrieve-ip-using-pushkar-kumar",
+  },
+  {
+    id: "8",
+    icon: "https://camo.githubusercontent.com/c823b22386c31361d21a4c8e76aa8c7f01b2119a0e4cca6fdcf267001735b94e/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f616e7369626c652e737667",
+    title: "Ancible Docker",
+    desc: "Ansible- Playbook to setup web Apache server on Docker",
+    link: "https://media-exp1.licdn.com/dms/image/C4E12AQENCS6fDbYybQ/article-cover_image-shrink_423_752/0/1598097235990?e=1639008000&v=beta&t=t628KwIabLtw9eI_n4Z0Bpu13r_TGmhst2d5XyPdi3g",
+    source:
+      "https://www.linkedin.com/pulse/ansible-playbook-configure-web-apache-server-pushkar-kumar",
+  },
+  {
+    id: "9",
+    icon: "https://camo.githubusercontent.com/986c248e8a100a55084cfb249ef904dcb0d6992b3ad36cc43455005301a902c9/687474703a2f2f73696d706c6569636f6e732e6f72672f69636f6e732f666c75747465722e737667",
+    title: "Flutter Music App",
+    desc: "Flutter App Development- Create a basic Music App with Play, Pause, Stop and Forward Buttons",
+    link: "https://media-exp1.licdn.com/dms/image/C4E12AQF4TL6q4t5cOg/article-cover_image-shrink_720_1280/0/1597226666433?e=1639008000&v=beta&t=baxcG0l_nOMojXP80TZkfqQy7kFaziyiBqE03OPxjBg",
+    source:
+      "https://www.linkedin.com/pulse/flutter-app-development-create-basic-music-play-pause-pushkar-kumar",
+  },
+];
